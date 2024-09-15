05928a8fe4a07a54cb9a6cb862ab073bccedad90
sebastian <sglau@wisc.edu>
2024-09-14 22:50:22 -0500

    initial commit

---
+++ AppData/Local/Google/AndroidStudio2024.1/tmp/intellij-git-editor-local.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"C:/Program Files/Android/Android Studio/jbr/bin/java" -cp "C:/Program Files/Android/Android Studio/plugins/vcs-git/lib/git4idea-rt.jar;C:/Program Files/Android/Android Studio/lib/externalProcess-rt.jar" git4idea.editor.GitRebaseEditorApp "$@"
---
+++ AppData/Local/Google/AndroidStudio2024.1/tmp/intellij-ssh-askpass-local.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"C:/Program Files/Android/Android Studio/jbr/bin/java" -cp "C:/Program Files/Android/Android Studio/lib/externalProcess-rt.jar" externalApp.nativessh.NativeSshAskPassApp "$@"
